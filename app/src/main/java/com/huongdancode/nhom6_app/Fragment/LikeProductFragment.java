4ff8a9648247f543ad1ae851429868c9d61339b6
GIGABYTE <vuplph17965@fpt.edu.vn>
2022-05-23 20:50:47 +0700

    Vu Commit

--- app/src/main/java/com/huongdancode/nhom6_app/Fragment/LikeProductFragment.java
+++ app/src/main/java/com/huongdancode/nhom6_app/Fragment/LikeProductFragment.java
@@ -15,7 +15,7 @@ public class LikeProductFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_like_product, container, false);
     }
 
     @Override
--- app/src/main/java/com/huongdancode/nhom6_app/Fragment/ProfileFragment.java
+++ app/src/main/java/com/huongdancode/nhom6_app/Fragment/ProfileFragment.java
@@ -15,7 +15,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
